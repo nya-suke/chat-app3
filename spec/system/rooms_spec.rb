772d3cd4429f96e7334fc856dff336e8016bc44b
nya-suke <hiromochiokui@gmail.com>
2021-08-12 21:55:28 +0900

    Update rooms_spec.rb

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'チャットルームの削除機能', type: :system do
   before do
-    @room_user = FactoryBot.create(:room_user)
+     @room_user = FactoryBot.create(:room_user)
   end
 
   it 'チャットルームを削除すると、関連するメッセージがすべて削除されていること' do
